70a61f54c585491b49f7911c15aa91ded6e0f201
clyde <oodennis20@gmail.com>
2020-01-10 15:26:05 +0300

    setting up the sign up page

---
+++ s/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth import logout
+from django.contrib.auth import login, authenticate
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+        return redirect('home') 
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
