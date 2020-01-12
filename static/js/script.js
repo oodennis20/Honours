d97667d1ad03de08b9e139d6f34573d91245d54d
clyde <oodennis20@gmail.com>
2020-01-12 11:35:45 +0300

    trying api

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,6 @@
-$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
+$(document).ready(function(){
+    $('form').submit(function(event){
+      event.preventDefault()
+    }) // End of submit event
 
-    trigger.click(function () {
-        hamburger_cross();
-    });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        }
-        else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
-
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-    });
-});
+}) // End of document ready function
