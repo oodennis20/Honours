ade2613314954e286a24dbb7abc4a52f6d39e5b5
clyde <oodennis20@gmail.com>
2020-01-13 04:56:15 +0300

    tests done

--- s/tests.py
+++ s/tests.py
@@ -18,9 +18,43 @@ class ProfileTest(TestCase):
         profile = Profile.get_profile()
         self.assertTrue(len(profile) > 0)
 
-    def test_find_profile(self):
-        self.profile.save()
-        profile = Profile.find_profile('clyde')
-        self.assertTrue(len(profile) > 0)
+   class ProjectTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='clyde')
+        self.profile = Profile.objects.create(user = self.user,bio = 'king',phone= 2356789)
+
+        self.project = Project.objects.create(posted_by = self.user, profile = self.profile, title = 'Gram', description='king', project_link= 'https://clydegram.herokuapp.com/')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))
+
+    def test_get_projects(self):
+        self.project.save()
+        project = Project.get_projects()
+        self.assertTrue(len(project) == 1)
+
+    def test_find_project(self):
+        self.project.save()
+        project = Project.find_project('gram')
+        self.assertTrue(len(project) > 0)
+
+class ReviewsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='clyde')
+
+        self.review= Reviews.objects.create(juror= self.user, design=5, usability=5,content=5,comment="dope" )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.review, Reviews))
+
+    def test_save_review(self):
+        self.assertTrue(isinstance(self.review,Reviews))
+
+    def test_get_reviews(self):
+        self.review.save()
+        review = Reviews.get_reviews()
+        self.assertTrue(len(review) == 1)
+
 
 
