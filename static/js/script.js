f1cab0669722ef55b37f05baa60ac139f58a61fa
clyde <oodennis20@gmail.com>
2020-01-13 03:04:30 +0300

    fixed nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $('form').submit(function(event){
-      event.preventDefault()
-    }) // End of submit event
+$('.avatar-container').on('click',function(){
+  if ($(this).find('.info').length <= 0) {
+    $(this).append('<div class="info"><div class="info-inner"></div></div>');
+  }
 
-}) // End of document ready function
+  var $info = $(this).find('.info'),
+      $inner= $(this).find('.info-inner'),
+      $p    = $(this).attr("class").match(/p-\w+/).toString().substring(2);
+
+  $inner.text($p+'%');
+  $info.toggleClass('js-active');
+});
+
+$('.avatar-container').last().trigger('click');
